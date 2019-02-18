1697ec513ef561c5bcf7111c180eb35232ab8d41
Steven Hair <stevenhair@users.noreply.github.com>
2019-02-18 17:31:42 -0500

    Read email (#7)
    * get email from, to, and subject
    
    * raed basic email
    
    * some more tests
    
    * workaround for Python 3.7
    
    * fixed lint errors
    
    * a bit of implementation
    
    * pylint is too opinionated
    
    * deal with HTML emails

---
+++ extractor/email_parser.py
@@ -0,0 +1,94 @@
+import email
+import logging
+import re
+from base64 import b64decode
+from typing import Tuple, Generator, Union
+
+from html2text import html2text
+
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+
+def clean_email_address(address: str) -> str:
+    """Returns the bare email address when the address is something like 'John Doe <jdoe@example.com>'"""
+
+    if '<' in address:
+        match = re.search(r'<(.*?)>', address)
+        if match:
+            return match.group(1).strip()
+        else:
+            LOGGER.warning('Unable to clean email address "%s" even though it contains "<"', address)
+    return address.strip()
+
+
+class EmailAttachment:
+    _attachment: email.message.EmailMessage
+
+    def __init__(self, attachment: email.message.EmailMessage):
+        self._attachment = attachment
+
+    @property
+    def body(self) -> Union[str, bytes]:
+        body = self._attachment.get_payload()
+        if self._attachment.get('Content-Transfer-Encoding') == 'base64':
+            body = b64decode(body)
+        if self.content_type == 'text/plain' and isinstance(body, bytes):
+            body = body.decode()
+        return body
+
+    @property
+    def content_type(self) -> str:
+        return self._attachment.get_content_type()
+
+    @property
+    def filename(self) -> str:
+        return self._attachment.get_filename()
+
+
+class ParsedEmail:
+    _message: email.message.EmailMessage
+
+    def __init__(self, message: email.message.EmailMessage):
+        super().__init__()
+        self._message = message
+
+    @property
+    def body(self) -> Union[str, None]:
+        body = None
+        body_part = self._message.get_body(('plain', 'related', 'html'))
+        if body_part:
+            if body_part.get_content_type() == 'text/html':
+                body = html2text(body_part.get_payload()).strip()
+            else:
+                body = body_part.get_payload().replace('\r', '')
+        return body
+
+    @property
+    def from_addr(self) -> str:
+        return clean_email_address(self._message['from'])
+
+    @property
+    def has_attachments(self) -> bool:
+        for _ in self.get_attachments():
+            return True
+        return False
+
+    @property
+    def subject(self) -> str:
+        return self._message['subject']
+
+    @property
+    def to_addr(self) -> Tuple[str]:
+        return tuple([clean_email_address(to) for to in self._message['to'].split(',')])
+
+    def get_attachments(self) -> Generator[EmailAttachment, None, None]:
+        # I'd love to just use iter_attachments() here, but it was throwing exceptions
+        for part in self._message.walk():
+            if part.get_content_disposition() == 'attachment':
+                yield EmailAttachment(part)
+
+    @classmethod
+    def from_bytes(cls, contents: bytes) -> 'ParsedEmail':
+        return cls(email.message_from_bytes(contents, _class=email.message.EmailMessage))
