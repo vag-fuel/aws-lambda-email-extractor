7d12cedd002874fdae4edc32d91930d674670d65
Steven Hair <me@stevenhair.com>
2021-02-15 15:26:41 -0500

    moto requires a default region

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,11 @@
 import boto3
+import os
 from moto import mock_s3
 
 
 @mock_s3
 def create_s3_object(bucket: str, key: str, contents: bytes) -> None:
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
     client = boto3.client('s3')
     client.create_bucket(Bucket=bucket)
     client.put_object(Body=contents, Bucket=bucket, Key=key)
